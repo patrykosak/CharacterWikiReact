e7507d6a2525a7e9703a145c75018ee4021788f8
patrykosak <pionchaoo@gmail.com>
2022-01-05 23:31:09 +0100

    first card appearance

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Cards = ({results}) => {
+const Cards = ({ results }) => {
+  let display;
+  if (results) {
+    display = results?.map((character) => {
+      const { id, name, image, location } = character;
+      return (
+        <div className="col-4" key={id}>
+          <div className="">
+            <img src={image} alt="" className="img-fluid " />
+            <div className="content">
+              <div className="fs-4 fw-bold mb-4">{name}</div>
+              <div className="fs-6">Last location</div>
+              <div className="fs-5">{location.name}</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  } else {
+    display = "No Characters Found";
+  }
+  return <>{display}</>;
+};
 
-    let display;
-    if(results){
-        display = results.map((character)=>(     
-        <div className='col-4'>
-        {character.name}
-        </div>)
-        )    
-}
-    else{display = "No Characters Found"}
-    return (
-        <>
-        {display}
-        </>
-    )
-}
-
-export default Cards
+export default Cards;
