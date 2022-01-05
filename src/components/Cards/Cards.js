1de5e0fa244d1cf96ff0d3fcdd7b2d0c17e5807f
patrykosak <pionchaoo@gmail.com>
2022-01-05 23:39:03 +0100

    add badge with character status and styled it

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,13 @@
 import React from "react";
+import styles from "./Cards.module.scss";
 
 const Cards = ({ results }) => {
   let display;
   if (results) {
     display = results?.map((character) => {
-      const { id, name, image, location } = character;
+      const { id, name, image, location, status } = character;
       return (
-        <div className="col-4" key={id}>
+        <div className="col-4 position-relative" key={id}>
           <div className="">
             <img src={image} alt="" className="img-fluid " />
             <div className="content">
@@ -15,6 +16,7 @@ const Cards = ({ results }) => {
               <div className="fs-5">{location.name}</div>
             </div>
           </div>
+          <div className={`${styles.badge} position-absolute badge bg-danger`}>{status}</div>
         </div>
       );
     });
