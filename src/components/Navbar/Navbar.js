2e80c8c5cae046ea700c19553b423798952887f4
patrykosak <pionchaoo@gmail.com>
2022-01-07 23:43:06 +0100

    navbar stylized

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,39 @@
 import React from 'react'
-
+import { NavLink, Link } from 'react-router-dom'
+import "../../App.css"
 const Navbar = () => {
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
 <div className='container'>
-  <a className="navbar-brand" href="#"><h1 className="fs-3 ubuntu navbar-brand">
+    <Link to="/" className="fs-3 ubuntu navbar-brand">
         Rick & Morty <span className="text-primary">WiKi</span>
-      </h1></a>
+    </Link>
+    <style jsx>
+        {`
+        button[aria-expanded="false"] > .close{
+            display: none;
+        }
+        button[aria-expanded="true"] > .open{
+            display: none;
+        }
+        `}
+    </style>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <i className='fas fa-bars open fw-bold text-dark'></i>
+    <i className='fas fa-times close fw-bold text-dark'></i>
   </button>
   <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Characters <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Episode</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Location</a>
-      </li>
-    </ul>
+    <div className="navbar-nav fs-5 ">
+      <NavLink activeClassName="active" to="/" className="nav-link">
+        Characters
+      </NavLink>
+      <NavLink to="/episodes" className="nav-link">
+        Episode
+      </NavLink>
+      <NavLink to="/location" className="nav-link">
+        Location
+      </NavLink>
+    </div>
   </div>
   </div>
 </nav>
