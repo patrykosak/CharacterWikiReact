b4e6a6cc92e130cd0f4c9263922b3ded7b8aaa5f
patrykosak <pionchaoo@gmail.com>
2022-01-08 19:55:15 +0100

    add protection to error if user pick choose instead of correct episode

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -4,7 +4,7 @@ const InputGroup = ({total, name, setID}) => {
   return (
     <div className="input-group mb-3">
       <select onChange={(e)=>{setID(e.target.value)}} className="form-select" id={name}>
-      <option selected>Choose...</option>
+      <option selected disabled>Choose...</option>
        {[...Array(total).keys()].map((x)=>{
            return <option value={x+1}>{name} - {x+1}</option>
        })}
