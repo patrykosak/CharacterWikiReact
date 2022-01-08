8827a3d6ce83ea9d647b11e283fdb80b5bfc398a
patrykosak <pionchaoo@gmail.com>
2022-01-08 20:34:44 +0100

    add location page

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
+import InputGroup from "../components/Filters/Category/InputGroup";
 
 const Location = () => {
-    return (
-        <div>
-            Locations
-        </div>
-    )
-}
+  const [info, setInfo] = useState([]);
+  const [id, setID] = useState(1);
+  const [results, setResults] = useState([]);
+  const {name,type,dimension} = info;
+  const api = `https://rickandmortyapi.com/api/location/${id}`;
+  useEffect(()=>{
+      (async function(){
+          const data = await fetch(api).then(res=>res.json())
+          console.log(data)
+          setInfo(data);
 
-export default Location
+          const a = await Promise.all(
+              data.residents.map((x)=>{
+                  return fetch(x).then(res=>res.json())
+              })
+              )
+              setResults(a);
+
+        })();
+  },[api]);
+
+  return (
+    <div className="container">
+      <div className="row mb-4">
+          <h1 className="text-center">Location : <span className="text-primary">{name === "" ? "Unknown" : name}</span> </h1>
+          <h4 className="text-center">Dimension: {dimension === "" ? "Unknown" : dimension}  </h4>
+          <h6 className="text-center">Type: {type === "" ? "Unknown" : type}  </h6>
+      </div>
+      <div className="row">
+          <div className="col-3 text-center">
+          <h4 className="text-center mb-4">
+        Pick Location
+        </h4>
+              <InputGroup name="Location" total={126} setID={setID} />
+          </div>
+          <div className="col-8">
+              <div className="row">
+                  <Cards results={results}/>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -6,7 +6,7 @@ const InputGroup = ({total, name, setID}) => {
       <select onChange={(e)=>{setID(e.target.value)}} className="form-select" id={name}>
       <option selected disabled>Choose...</option>
        {[...Array(total).keys()].map((x)=>{
-           return <option value={x+1}>{name} - {x+1}</option>
+           return <option key={x+1} value={x+1}>{name} - {x+1}</option>
        })}
       </select>
     </div>
